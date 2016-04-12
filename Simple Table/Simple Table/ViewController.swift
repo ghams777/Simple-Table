f0d593821218b29e1dfaabaca4999c5b67767ca9
Managam Silalahi <managam.silalahi@gmail.com>
2016-04-12 10:10:44 +0700

    Add Thumbnail to Table View

--- Simple Table/Simple Table/ViewController.swift
+++ Simple Table/Simple Table/ViewController.swift
@@ -36,6 +36,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let cellIdentifier = "Cell"
         let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath)
         cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: "restaurant")
             
             
         return cell
